a547d3a6b05f41b88e939efe33ce0f07577b2967
nomoneynolife <myourlove5@gmail.com>
2026-03-13 14:24:57 +0800

    自动化改进

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +15,7 @@ using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ThreeKingdomsHeroes;
 
@@ -20,11 +25,133 @@ namespace ThreeKingdomsHeroes;
 public partial class MainWindow : Window
 {
     private List<Hero> heroes;
+    private bool isF12Active = false;
+    private bool isAutoHammerEnabled = false;
+    private bool isAutoUltEnabled = false;
+    private System.Threading.Timer hammerTimer;
+    private System.Threading.Timer ultTimer;
+    private const int HOTKEY_ID = 9000;
+    private const uint MOD_NONE = 0x0000;
+    private const uint VK_F12 = 0x7B;
+    private const uint VK_F1 = 0x70;
+
+    // 全局键盘钩子相关
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private static LowLevelKeyboardProc _proc;
+    private static IntPtr _hookID = IntPtr.Zero;
+
+    [DllImport("user32.dll")]
+    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    private const uint MOUSEEVENTF_RIGHTDOWN = 0x08;
+    private const uint MOUSEEVENTF_RIGHTUP = 0x10;
+    private const uint KEYEVENTF_KEYDOWN = 0x00;
+    private const uint KEYEVENTF_KEYUP = 0x02;
+
+    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
     public MainWindow()
     {
         InitializeComponent();
         InitializeHeroes();
+        // 设置全局键盘钩子
+        _proc = HookCallback;
+        _hookID = SetHook(_proc);
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        var source = PresentationSource.FromVisual(this) as System.Windows.Interop.HwndSource;
+        if (source != null)
+        {
+            // 设置窗口样式，确保即使在后台也能接收消息
+            source.AddHook(HwndHook);
+            // 使用当前窗口句柄注册热键
+            bool success = RegisterHotKey(source.Handle, HOTKEY_ID, MOD_NONE, VK_F12);
+            // 调试信息
+            System.Diagnostics.Debug.WriteLine($"热键注册成功: {success}");
+        }
+    }
+
+    private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        const int WM_HOTKEY = 0x0312;
+        if (msg == WM_HOTKEY && wParam.ToInt32() == HOTKEY_ID)
+        {
+            // 在UI线程上执行操作
+            this.Dispatcher.Invoke(() =>
+            {
+                ToggleF12Status();
+            });
+            handled = true;
+        }
+        return IntPtr.Zero;
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        // 移除全局键盘钩子
+        UnhookWindowsHookEx(_hookID);
+        
+        var source = PresentationSource.FromVisual(this) as System.Windows.Interop.HwndSource;
+        if (source != null)
+        {
+            bool success = UnregisterHotKey(source.Handle, HOTKEY_ID);
+            System.Diagnostics.Debug.WriteLine($"热键注销成功: {success}");
+        }
+        hammerTimer?.Dispose();
+        ultTimer?.Dispose();
+        base.OnClosed(e);
+    }
+
+    private static IntPtr SetHook(LowLevelKeyboardProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+            if (vkCode == VK_F12)
+            {
+                // 在UI线程上执行操作
+                this.Dispatcher.Invoke(() =>
+                {
+                    ToggleF12Status();
+                });
+            }
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
     }
 
     private void InitializeHeroes()
@@ -162,6 +289,144 @@ public partial class MainWindow : Window
     private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
         this.DragMove();
+    }
+
+    private void ToggleF12Status()
+    {
+        isF12Active = !isF12Active;
+        UpdateF12StatusText();
+        
+        if (isF12Active)
+        {
+            if (isAutoHammerEnabled)
+            {
+                StartHammerTimer();
+            }
+            if (isAutoUltEnabled)
+            {
+                StartUltTimer();
+            }
+        }
+        else
+        {
+            StopHammerTimer();
+            StopUltTimer();
+        }
+    }
+
+    private void UpdateF12StatusText()
+    {
+        if (isF12Active)
+        {
+            tbF12Status.Text = "F12状态: 已启动";
+            tbF12Status.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4CAF50"));
+        }
+        else
+        {
+            tbF12Status.Text = "F12状态: 未启动";
+            tbF12Status.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#666666"));
+        }
+    }
+
+    private void StartHammerTimer()
+    {
+        if (hammerTimer == null)
+        {
+            hammerTimer = new System.Threading.Timer(CheckProcessAndClick, null, 0, 5000);
+        }
+    }
+
+    private void StopHammerTimer()
+    {
+        if (hammerTimer != null)
+        {
+            hammerTimer.Dispose();
+            hammerTimer = null;
+        }
+    }
+
+    private void StartUltTimer()
+    {
+        if (ultTimer == null)
+        {
+            ultTimer = new System.Threading.Timer(CheckProcessAndPressF1, null, 0, 500);
+        }
+    }
+
+    private void StopUltTimer()
+    {
+        if (ultTimer != null)
+        {
+            ultTimer.Dispose();
+            ultTimer = null;
+        }
+    }
+
+    private void CheckProcessAndClick(object state)
+    {
+        // 检查TDClient.bin进程
+        bool processExists = Process.GetProcessesByName("TDClient").Length > 0 || Process.GetProcessesByName("TDClient.bin").Length > 0;
+        
+        if (processExists)
+        {
+            // 模拟右键点击
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+            Thread.Sleep(50);
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+        }
+    }
+
+    private void CheckProcessAndPressF1(object state)
+    {
+        // 检查TDClient.bin进程
+        bool processExists = Process.GetProcessesByName("TDClient").Length > 0 || Process.GetProcessesByName("TDClient.bin").Length > 0;
+        
+        if (processExists)
+        {
+            // 模拟F1键按下
+            keybd_event((byte)VK_F1, 0, KEYEVENTF_KEYDOWN, 0);
+            Thread.Sleep(50);
+            keybd_event((byte)VK_F1, 0, KEYEVENTF_KEYUP, 0);
+        }
+    }
+
+    private void cbAutoHammer_Checked(object sender, RoutedEventArgs e)
+    {
+        isAutoHammerEnabled = true;
+        if (isF12Active)
+        {
+            StartHammerTimer();
+        }
+    }
+
+    private void cbAutoHammer_Unchecked(object sender, RoutedEventArgs e)
+    {
+        isAutoHammerEnabled = false;
+        StopHammerTimer();
+    }
+
+    private void cbAutoUlt_Checked(object sender, RoutedEventArgs e)
+    {
+        isAutoUltEnabled = true;
+        if (isF12Active)
+        {
+            StartUltTimer();
+        }
+    }
+
+    private void cbAutoUlt_Unchecked(object sender, RoutedEventArgs e)
+    {
+        isAutoUltEnabled = false;
+        StopUltTimer();
+    }
+
+    private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == Key.F12)
+        {
+            ToggleF12Status();
+            e.Handled = true;
+        }
     }
 }
 
